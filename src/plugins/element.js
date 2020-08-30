aafecd10798cf13cd568d53a4528ba65eff7a77b
晚安北京 <120907030@qq.com>
2020-08-30 21:10:33 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-
+//花括号表示按需导入
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+//导入后需使用
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+//Message必须全局挂载
+Vue.prototype.$message = Message
